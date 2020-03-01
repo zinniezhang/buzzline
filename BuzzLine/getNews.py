218b2dc66f47f7056190a9ffe27b43a9f29c19ca
sithara14 <sithara14@github.com>
2020-02-29 19:11:55 -0600

    array of sentiment

--- BuzzLine/getNews.py
+++ BuzzLine/getNews.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from textblob import TextBlob
 
 input = "tesla"
 
@@ -7,4 +8,10 @@ newsapi ="22d21f2234d74ec7b27482cbe70fcde8"
 url = "https://newsapi.org/v2/everything?q="+input+"&apiKey="+newsapi+""
 newsObject  = requests.get(url).json()
 totalArticles = newsObject["articles"]
+sentimentAnalysis = []
+for ar in totalArticles:
+    articleSent = TextBlob(ar["content"])
+    sentimentAnalysis.append(articleSent.sentiment)
+
+
 
