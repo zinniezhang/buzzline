0f37ee039a340b6770a8ec2c0d8004d485197cfd
zinniezhang <zinnie.zhang@gmail.com>
2020-03-04 12:33:35 -0600

    Change after hackathon

--- BuzzLine/getNews.py
+++ BuzzLine/getNews.py
@@ -1,11 +1,14 @@
 import requests
 import json
+import matplotlib
 import matplotlib.pyplot as plt
 from textblob import TextBlob
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
 from matplotlib.pyplot import figure
 
+
+matplotlib.use("agg")
 
 # Your key here
 key = 'H2TRXQR5HFYF1EVK'
@@ -16,34 +19,34 @@ ti = TechIndicators(key)
 
 def graphTicker(tickerSymbol):
     url = "https://newsapi.org/v2/everything?q="+tickerSymbol+"&apiKey="+newsapi+""
-    newsObject  = requests.get(url).json()
-    totalArticles = newsObject["articles"]
-    publishingDate = []
-    sentimentAnalysis = []
-    for ar in totalArticles:
-        publishingDate.append(ar["publishedAt"][0:10])
-        articleSent = TextBlob(ar["content"])
-        sentimentAnalysis.append(articleSent.sentiment.polarity)
+#    newsObject  = requests.get(url).json()
+#    totalArticles = newsObject["articles"]
+#    publishingDate = []
+#    sentimentAnalysis = []
+#    for ar in totalArticles:
+#        publishingDate.append(ar["publishedAt"][0:10])
+#        articleSent = TextBlob(ar["content"])
+#        sentimentAnalysis.append(articleSent.sentiment.polarity)
 
 
     # Get the data, returns a tuple
     # aapl_data is a pandas dataframe, aapl_meta_data is a dict
-    aapl_data, aapl_meta_data = ts.get_daily(symbol=tickerSymbol)
-    # aapl_sma is a dict, aapl_meta_sma also a dict
-    aapl_sma, aapl_meta_sma = ti.get_sma(symbol=tickerSymbol)
+#    aapl_data, aapl_meta_data = ts.get_daily(symbol=tickerSymbol)
+#    # aapl_sma is a dict, aapl_meta_sma also a dict
+#    aapl_sma, aapl_meta_sma = ti.get_sma(symbol=tickerSymbol)
 
     # print(aapl_data)
 
     # Visualization
-    figure(num=None, figsize=(15, 6), dpi=80, facecolor='w', edgecolor='k')
-    print(aapl_data)
-    aapl_data['4. close'].plot()
-    plt.tight_layout()
-    plt.grid()
-    plt.savefig('stocks/' + tickerSymbol + '.png')
-    plt.show()
-    plt.scatter(publishingDate, sentimentAnalysis)
-    plt.savefig('sentimentValue.png')
-    return 'stocks/' + tickerSymbol + '.png'
+#    figure(num=None, figsize=(15, 6), dpi=80, facecolor='w', edgecolor='k')
+#    print(aapl_data)
+#    aapl_data['4. close'].plot()
+#    plt.tight_layout()
+#    plt.grid()
+#    plt.savefig('stocks/' + tickerSymbol + '.png')
+#    plt.show()
+#    plt.scatter(publishingDate, sentimentAnalysis)
+#    plt.savefig('sentimentValue.png')
+    return 'images/' + tickerSymbol + '.png'
     
 
--- BuzzLine/server.py
+++ BuzzLine/server.py
@@ -1,56 +1,89 @@
-#!/usr/bin/python
-from http.server import BaseHTTPRequestHandler,HTTPServer, SimpleHTTPRequestHandler
+##!/usr/bin/python
+#from http.server import BaseHTTPRequestHandler,HTTPServer, SimpleHTTPRequestHandler
 from getNews import graphTicker
-import os
+#import os
+#
+#
+#PORT_NUMBER = 8080
+#
+##This class will handles any incoming request from
+##the browser 
+#class myHandler(BaseHTTPRequestHandler):
+#	
+#	#Handler for the GET requests
+#	
+#	def do_GET(self):
+#		self.send_response(200)
+#		self.send_header('Content-type','text/plain')
+#		self.end_headers()
+##		message = cow.Cowacter().milk('Hello from Python from a ZEIT Now Serverless Function!')
+#		message = graphTicker(self.path[1:])
+#		self.wfile.write(message.encode())
+#		return
+#
+##	def do_GET(self):
+##		self.wfile.write(bytes("hello", "utf-8"))
+##		self.wfile.close()
+##		return
+##		print(self.path[1:])
+##		self.send_header('Content-Type', 'text/plain')
+##		self.end_headers()
+##		if False: # os.path.exists(self.path[1:]):
+###			with open(self.path[1:], 'rb') as f:
+###				self.send_response(200)
+###				if self.path.split('.')[1] == 'png':
+###					self.send_header('Content-Type', 'image/png')
+###				if self.path.split('.')[1] == 'html':
+###					self.send_header('Content-Type', 'text/html')
+###				if self.path.split('.')[1] == 'css':
+###					self.send_header('Content-Type', 'text/css')
+###				if self.path.split('.')[1] == 'jpg' or self.path.split('.')[1] == 'jpeg':
+###					self.send_header('Content-Type', 'image/jpeg')
+###				self.end_headers()
+###				self.wfile.write(f.read())
+###		# Send the html message
+##			print("hi")
+##		elif self.path == '/favicon.ico':
+##			self.send_response(404)
+##		else:
+##			self.wfile.write(bytes('http://localhost:8080/' + graphTicker(self.path[1:]), 'utf-8'))
+##		return
+#
+#try:
+#	#Create a web server and define the handler to manage the
+#	#incoming request
+#	server = HTTPServer(('127.0.0.1', PORT_NUMBER), myHandler)
+#	print('Started httpserver on port ' , PORT_NUMBER)
+#	
+#	#Wait forever for incoming htto requests
+#	server.serve_forever()
+#
+#except KeyboardInterrupt:
+#	print('^C received, shutting down the web server')
+#	server.socket.close()
+
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 
 
-PORT_NUMBER = 8080
+app = Flask(__name__,static_url_path='/images', static_folder="stocks")
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app)
 
-#This class will handles any incoming request from
-#the browser 
-class myHandler(BaseHTTPRequestHandler):
+
+@app.route('/<stock>')
+@cross_origin()
+def hello_world(stock):
+    return jsonify(url = "http://localhost:5000/" + graphTicker(stock))
+
+
+#@app.route("/images")
+#@cross_origin()
+#def return_image():
 	
-	#Handler for the GET requests
-	def do_GET(self):
-		if os.path.exists(self.path[1:]):
-			with open(self.path[1:], 'rb') as f:
-				self.send_response(200)
-				if self.path.split('.')[1] == 'png':
-					self.send_header('Content-Type', 'image/png')
-				if self.path.split('.')[1] == 'html':
-					self.send_header('Content-Type', 'text/html')
-				if self.path.split('.')[1] == 'css':
-					self.send_header('Content-Type', 'text/css')
-				if self.path.split('.')[1] == 'jpg' or self.path.split('.')[1] == 'jpeg':
-					self.send_header('Content-Type', 'image/jpeg')
-				self.end_headers()
-				self.wfile.write(f.read())
-		# Send the html message
-		elif self.path == '/favicon.ico':
-			self.send_response(404)
-		else:
-			self.wfile.write(bytes('http://localhost:8080/' + graphTicker(self.path[1:]), 'utf-8'))
-		return
 
-try:
-	#Create a web server and define the handler to manage the
-	#incoming request
-	server = HTTPServer(('', PORT_NUMBER), myHandler)
-	print('Started httpserver on port ' , PORT_NUMBER)
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 	
-	#Wait forever for incoming htto requests
-	server.serve_forever()
-
-except KeyboardInterrupt:
-	print('^C received, shutting down the web server')
-	server.socket.close()
-    
-import socketserver
-
-PORT = 8000
-web_dir = os.path.join(os.path.dirname(__file__), 'stocks')
-os.chdir(web_dir)
-Handler = SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(("", PORT), Handler)
-print("serving at port", PORT)
-httpd.serve_forever()
